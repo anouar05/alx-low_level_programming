e60bd9f540f2c4dd628bf3947c8e03757652ccaa
Anouar <anouar.work.2000@gmail.com>
2023-08-15 19:38:58 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,20 +5,25 @@
  * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
  */
 int print_sign(int n)
-
-      if (n > 0)
-      {
-             _putchar(43);
-             return (1);
-      }
-      else if (n < 0)
-      {
-            _putchar(45);
-            return (-1);
-      }
-      else
-      {
-            _putchar(48);
-            return (0);
-      }
-      _putchar('\n');
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
